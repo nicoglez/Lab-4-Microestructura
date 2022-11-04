017fcea3d163f94405fbd0421ee74476d486e925
Nicolas Gonzalez <nicolasvazquez1@outlook.es>
2022-11-04 17:19:31 -0600

    stylized graphics

--- visualizations.py
+++ visualizations.py
@@ -34,74 +34,131 @@ def line_chart(coin: str, variable: str, data: pd.DataFrame):
 
 # Grafica de total volume de n exchanges
 def total_volume_chart(coin: str, data: pd.DataFrame):
-    # Siempre sera total volume
-    variable = "total_volume"
-    # Cambiar formato de pandas para no tener errores
-    pd.options.mode.chained_assignment = None  # default='warn'
-    # Seleccionar solo la data de la moneda que necesitamos
-    data_coin = data[data["coin"] == coin]
-    # Obtener dia de operacion
-    day = data_coin.iloc[0, 0][0:10]
+    # Obtener data de la moneda a graficar
+    data_coin = data[data["coin"]==coin]
     # Obtener set de mercado
     market = list(set(data["exchange"]))
 
+    # Obtener dia de operacion
+    day = data_coin.iloc[0,0][0:10]
+
     # Hacer temp con informacion de cada mercado y su fecha
-    temp = pd.DataFrame(data_coin[data_coin["exchange"] == market[0]]["timeStamp"])
-    temp["timeStamp"] = [i[12:] for i in temp["timeStamp"]]
+    data_coin["timeStamp"] = [i[12:] for i in data_coin["timeStamp"]]
 
-    # Estandarizar fechas
-    lista = []
-    for i in range(len(market)):
-        lista.append([i for i in temp.values])
-    l = list(temp["timeStamp"].values)
-    k = l
-    for i in range(len(market) - 1):
-        l = l + k
-    data_coin["timeStamp"] = l
+    # Inicializar figura
+    fig = make_subplots(rows=1, cols=3,
+                        shared_yaxes=True,
+                        subplot_titles = (f"{market[0]}",
+                                          f"{market[1]}",
+                                          f"{market[2]}"))
 
-    # Graficar
-    fig = px.line(data_coin, x='timeStamp', y=data_coin[variable], facet_col="exchange")
-    fig.update_layout(title=f"{variable} {coin}", xaxis_title=f"timeStamp ({day})", yaxis_title=variable)
-    fig.update_traces(line_color='black')
+    # Figura 1
+    fig.add_trace(go.Bar(
+        y= data_coin[data_coin["exchange"]==market[0]]["total_volume"],
+        x= data_coin[data_coin["exchange"]==market[0]]["timeStamp"],
+        marker_color="black", width = 2,
+        name=f"total_volume {market[0]}"),
+        row=1, col=1)
 
-    # Mostrar grafica
+    # Figura 2
+    fig.add_trace(go.Bar(
+        y= data_coin[data_coin["exchange"]==market[1]]["total_volume"],
+        x= data_coin[data_coin["exchange"]==market[0]]["timeStamp"],
+        marker_color="red", width = 2,
+        name=f"tota_volume {market[1]}",),
+        row=1, col=2)
+
+    # Figura 3
+    fig.add_trace(go.Bar(
+        y= data_coin[data_coin["exchange"]==market[2]]["total_volume"],
+        x= data_coin[data_coin["exchange"]==market[0]]["timeStamp"],
+        marker_color="navy", width = 2,
+        name=f"total_volume {market[2]}",),
+        row=1, col=3)
+
+    # Mostrar figura
+    fig.update_layout(title_text=f"Total Volume {coin}")
+    fig.update_yaxes(title_text="Total Volume", row=1, col=1)
+    fig.update_xaxes(title_text=f"Time Stamp ({day})")
     fig.show()
 
 
-# Histograma de Ask y Bids
-def ask_bid_histograms(coin: str, data: pd.DataFrame):
-    # Cambiar formato de pandas para no tener errores
-    pd.options.mode.chained_assignment = None  # default='warn'
-    # Seleccionar solo la data de la moneda que necesitamos
+# Grafica de subplots de niveles por mercado segun una moneda
+def ask_bid_volume(coin: str, data: pd.DataFrame):
+    # Obtener data de la moneda a graficar
     data_coin = data[data["coin"] == coin]
-    # Obtener dia de operacion
-    day = data_coin.iloc[0, 0][0:10]
+
     # Obtener set de mercado
     market = list(set(data["exchange"]))
 
+    # Obtener dia de operacion
+    day = data_coin.iloc[0, 0][0:10]
+
     # Hacer temp con informacion de cada mercado y su fecha
-    temp = pd.DataFrame(data_coin[data_coin["exchange"] == market[0]]["timeStamp"])
-    temp["timeStamp"] = [i[12:] for i in temp["timeStamp"]]
+    data_coin["timeStamp"] = [i[12:] for i in data_coin["timeStamp"]]
 
-    # Estandarizar fechas
-    lista = []
-    for i in range(len(market)):
-        lista.append([i for i in temp.values])
-    l = list(temp["timeStamp"].values)
-    k = l
-    for i in range(len(market) - 1):
-        l = l + k
-    data_coin["timeStamp"] = l
+    # Inicializar figura
+    fig = make_subplots(rows=2, cols=3,
+                        shared_yaxes=True,
+                        subplot_titles=(f"ask {market[0]}",
+                                        f"ask {market[1]}",
+                                        f"ask {market[2]}",
+                                        f"bid {market[0]}",
+                                        f"bid {market[1]}",
+                                        f"bid {market[2]}"))
 
-    # Grafica 1 Ask
-    fig = px.histogram(data_coin, x='timeStamp', y=data_coin["ask_volume"], facet_col="exchange")
-    fig.update_layout(title=f"ask volume {coin}", xaxis_title=f"timeStamp ({day})", yaxis_title="ask_volume")
-    # Grafica 2. Bid
-    fig2 = px.histogram(data_coin, x='timeStamp', y=data_coin["bid_volume"], facet_col="exchange")
-    fig2.update_layout(title=f"bid volume {coin}", xaxis_title=f"timeStamp ({day})", yaxis_title="bid_volume")
+    # Figura 1 BID Y ASK
+    fig.add_trace(go.Bar(
+        y=data_coin[data_coin["exchange"] == market[0]]["ask_volume"],
+        x=data_coin[data_coin["exchange"] == market[0]]["timeStamp"],
+        marker_color="navy", width=3,
+        name=f"ask {market[0]}"),
+        row=1, col=1)
 
-    # Mostrar graficas
-    fig.show(), fig2.show()
+    fig.add_trace(go.Bar(
+        y=data_coin[data_coin["exchange"] == market[0]]["bid_volume"],
+        x=data_coin[data_coin["exchange"] == market[0]]["timeStamp"],
+        name=f"bid {market[0]}", width=3,
+        marker_color="green"),
+        row=2, col=1
+    )
+
+    # Figura 2 BID Y ASK
+    fig.add_trace(go.Bar(
+        y=data_coin[data_coin["exchange"] == market[1]]["ask_volume"],
+        x=data_coin[data_coin["exchange"] == market[0]]["timeStamp"],
+        marker_color="navy", width=5,
+        name=f"ask {market[1]}"),
+        row=1, col=2)
+    fig.add_trace(go.Bar(
+        y=data_coin[data_coin["exchange"] == market[1]]["bid_volume"],
+        x=data_coin[data_coin["exchange"] == market[0]]["timeStamp"],
+        name=f"bid {market[1]}", width=5,
+        marker_color="green"),
+        row=2, col=2
+    )
+
+    # Figura 3 BID Y ASK
+    fig.add_trace(go.Bar(
+        y=data_coin[data_coin["exchange"] == market[2]]["ask_volume"],
+        x=data_coin[data_coin["exchange"] == market[0]]["timeStamp"],
+        marker_color="navy", width=3,
+        name=f"ask {market[2]}", ),
+        row=1, col=3)
+    fig.add_trace(go.Bar(
+        y=data_coin[data_coin["exchange"] == market[2]]["bid_volume"],
+        x=data_coin[data_coin["exchange"] == market[0]]["timeStamp"],
+        name=f"bid {market[2]}",
+        marker_color="green", width=3),
+        row=2, col=3
+    )
+
+    # Mostrar figura
+    fig.update_yaxes(title_text="Volume", row=1, col=1)
+    fig.update_yaxes(title_text="Volume", row=2, col=1)
+    fig.update_xaxes(title_text=f"Time Stamp ({day})")
+    fig.update_layout(title_text=f"Bid & Ask Volume {coin}", width=1000, height=800)
+    fig.show()
 
 
 # Grafica de subplots de niveles por mercado segun una moneda
@@ -114,21 +171,24 @@ def levels_markets(coin: str, data: pd.DataFrame):
     # Inicializar figura
     fig = make_subplots(rows=1, cols=3,
                         shared_yaxes=True,
-                        subplot_titles = (f"Levels {market[0]} {coin}", f"Levels {market[1]} {coin}", f"Levels {market[2]} {coin}"))
+                        subplot_titles = (f"{market[0]}",
+                                          f"{market[1]}",
+                                          f"{market[2]}"))
 
     # Figura 1 BID Y ASK
     fig.add_trace(go.Bar(
         y= np.array(sorted(data_coin[data_coin["exchange"]==market[0]]["ask"])).astype(str),
         x= data_coin[data_coin["exchange"]==market[0]]["ask_volume"],
-        orientation='h',
-        marker_color="black",
+        orientation='h', width=3,
+        marker_color="navy",
         name=f"ask {market[0]}"),
         row=1, col=1)
     fig.add_trace(go.Bar(
         y= np.array(sorted(data_coin[data_coin["exchange"]==market[0]]["bid"])).astype(str),
         x= data_coin[data_coin["exchange"]==market[0]]["bid_volume"],
-        orientation='h', name=f"bid {market[0]}",
-        marker_color="salmon"),
+        orientation='h', width=3,
+        name=f"bid {market[0]}",
+        marker_color="red"),
         row=1, col=1
     )
 
@@ -136,13 +196,16 @@ def levels_markets(coin: str, data: pd.DataFrame):
     fig.add_trace(go.Bar(
         y= np.array(sorted(data_coin[data_coin["exchange"]==market[1]]["ask"])).astype(str),
         x= data_coin[data_coin["exchange"]==market[1]]["ask_volume"],
-        orientation='h', marker_color="blue", name=f"ask {market[1]}",),
+        orientation='h', width=3,
+        marker_color="navy",
+        name=f"ask {market[1]}",),
         row=1, col=2)
     fig.add_trace(go.Bar(
         y= np.array(sorted(data_coin[data_coin["exchange"]==market[1]]["bid"])).astype(str),
         x= data_coin[data_coin["exchange"]==market[1]]["bid_volume"],
-        orientation='h', name=f"bid {market[1]}",
-        marker_color="green"),
+        orientation='h', width=3,
+        name=f"bid {market[1]}",
+        marker_color="red"),
         row=1, col=2
     )
 
@@ -150,17 +213,21 @@ def levels_markets(coin: str, data: pd.DataFrame):
     fig.add_trace(go.Bar(
         y= np.array(sorted(data_coin[data_coin["exchange"]==market[2]]["ask"])).astype(str),
         x= data_coin[data_coin["exchange"]==market[2]]["ask_volume"],
-        orientation='h', marker_color="navy", name=f"ask {market[2]}",),
+        orientation='h', width=3,
+        marker_color="navy",
+        name=f"ask {market[2]}",),
         row=1, col=3)
     fig.add_trace(go.Bar(
         y= np.array(sorted(data_coin[data_coin["exchange"]==market[2]]["bid"])).astype(str),
         x= data_coin[data_coin["exchange"]==market[2]]["bid_volume"],
-        orientation='h', name=f"bid {market[2]}",
+        orientation='h', width=3,
+        name=f"bid {market[2]}",
         marker_color="red"),
         row=1, col=3
     )
 
     # Mostrar figura
+    fig.update_layout(title_text=f"Levels {coin}")
     fig.update_yaxes(title_text="Price", row=1, col=1)
     fig.update_xaxes(title_text="Volume")
     fig.show()
