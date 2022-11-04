870dff730d9fd2eb1e650374dca827cd74577bab
Nicolas Gonzalez <nicolasvazquez1@outlook.es>
2022-11-04 17:18:46 -0600

    update sort

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ from functions import ccxt_getter
 from functions import pd_to_dict
 
 # Definir diccionario con mercados de donde descarga la info y sus metodos para descargar
-markets = {"binance": ccxt.binance(), "kraken": ccxt.kraken(), "currencycom": ccxt.currencycom()}
+markets = {"kraken": ccxt.kraken(), "currencycom": ccxt.currencycom(), "binance": ccxt.binance()}
 # Definir las monedas y sus cambios a bajar
 coins = ['BTC/USDT', 'BTC/EUR', "ETH/USDT"]
 
@@ -13,5 +13,5 @@ coins = ['BTC/USDT', 'BTC/EUR', "ETH/USDT"]
 #ccxt_getter(markets=markets, coins=coins, minutes_fetch=60, filepath="files/data_Lab_4.xlsx")
 
 # Leer xlsx generado
-data = pd.read_excel("files/data_Lab_4.xlsx", engine='openpyxl').sort_values(by="exchange")
+data = pd.read_excel("files/data_Lab_4.xlsx", engine='openpyxl').sort_values(by="timeStamp")
 data_dict = pd_to_dict(data)
